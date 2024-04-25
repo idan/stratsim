2521b8723cfd3c6bb3e021eff044375a3ec9f837
Idan Gazit <idan@gazit.me>
2024-04-24 21:51:18 -0700

    Use a strat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     import Keysequence from "$lib/keysequence.svelte";
+    import { stratagems } from "$lib/strats";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Keysequence />
+<Keysequence strat={stratagems[0]}/>
